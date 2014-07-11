6cc015575105541b83473fa30e8481b850f4ceb8
IceHaron <ice_haron@mail.ru>
2014-07-11 15:52:23 +0400

    Core v.0.6.11.50
    Запрещаем вывод ошибок чтобы на почту не валился спам.

--- php/checkmail.php
+++ php/checkmail.php
@@ -1,5 +1,5 @@
 <?
-
+ini_set('display_errors', 0);
 $rootfolder = isset($_SERVER['HOME']) ? $_SERVER['HOME'].'/elysium' : $_SERVER['DOCUMENT_ROOT'];
 REQUIRE_ONCE($rootfolder . '/settings.php');
 REQUIRE_ONCE('classes/db.php');
