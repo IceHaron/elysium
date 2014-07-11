6020ca990af6b5eb477d42dc94a15e8cddaa564e
IceHaron <ice_haron@mail.ru>
2014-07-11 15:41:20 +0400

    Core v.0.6.10.49
    Забыл создать экземпляр

--- php/checkmail.php
+++ php/checkmail.php
@@ -4,5 +4,6 @@ $rootfolder = isset($_SERVER['HOME']) ? $_SERVER['HOME'].'/elysium' : $_SERVER['
 REQUIRE_ONCE($rootfolder . '/settings.php');
 REQUIRE_ONCE('classes/db.php');
 REQUIRE_ONCE('classes/mail.php');
+$db = new db();
 $mailer = new mail();
 $mailer->receive();
