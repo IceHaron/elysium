9189af42b74e92adb067d449c28734cfad13d43d
IceHaron <ice_haron@mail.ru>
2014-07-11 15:38:50 +0400

    Core v.0.6.9.48
    + Класс базы

--- php/checkmail.php
+++ php/checkmail.php
@@ -2,6 +2,7 @@
 
 $rootfolder = isset($_SERVER['HOME']) ? $_SERVER['HOME'].'/elysium' : $_SERVER['DOCUMENT_ROOT'];
 REQUIRE_ONCE($rootfolder . '/settings.php');
+REQUIRE_ONCE('classes/db.php');
 REQUIRE_ONCE('classes/mail.php');
 $mailer = new mail();
 $mailer->receive();
