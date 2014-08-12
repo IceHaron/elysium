71918cb9635ac68184d056b7de04d81297ab6ba0
IceHaron <ice_haron@mail.ru>
2014-08-12 17:27:18 +0400

    Minor
    Счетчик Ли.ру и смена папки

--- php/checkmail.php
+++ php/checkmail.php
@@ -1,6 +1,6 @@
 <?
 ini_set('display_errors', 0);
-$rootfolder = isset($_SERVER['HOME']) ? $_SERVER['HOME'].'/elysium' : $_SERVER['DOCUMENT_ROOT'];
+$rootfolder = isset($_SERVER['HOME']) ? $_SERVER['HOME'].'/elysiumgame' : $_SERVER['DOCUMENT_ROOT'];
 REQUIRE_ONCE($rootfolder . '/settings.php');
 REQUIRE_ONCE('classes/db.php');
 REQUIRE_ONCE('classes/mail.php');
