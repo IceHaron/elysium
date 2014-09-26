340ffcd0a494b1abe03ea9f8407b9927113c86a2
IceHaron <ice_haron@mail.ru>
2014-09-26 10:43:14 +0400

    WIP#2 final

--- php/controllers/payaccept.php
+++ php/controllers/payaccept.php
@@ -8,7 +8,7 @@ $r = $db->query($q);
 
 if ($r !== NULL) {
 	$signPost = roboSignature(array($OutSum, $InvId), 'pay');
-	$signBase = roboSignature(array($r[0]['topay'], $InvId), 'pay');
+	$signBase = roboSignature(array($r[0]['topay'].'0000', $InvId), 'pay');
 	$checkSum = ((float)$OutSum === (float)$r[0]['topay']);
 	$checkPaid = ($r[0]['paid'] == 0);
 	$checkSignature = ($signPost == $signBase && $signPost == $SignatureValue && $signBase == $SignatureValue);
@@ -20,11 +20,11 @@ if ($r !== NULL) {
 		$message .= '<br/>Спасибо за покупку изюма! Он будет выдан вам как только платеж будет завершен. <a href="/lk">Уйти в ЛК</a>';
 
 	} else {
-		var_dump(trim($OutSum,'0'), $OutSum, $r[0]['topay']);
+		// var_dump(trim($OutSum,'0'), $OutSum, $r[0]['topay']);
 		if (!$checkSum) $message .= '<br/>Не совпадают суммы платежа';
 		// var_dump($r[0]['paid']);
 		if (!$checkPaid) $message .= '<br/>Транзакция уже оплачена';
-		var_dump($signBase, $signPost, $SignatureValue);
+		// var_dump($signBase, $signPost, $SignatureValue);
 		if (!$checkSignature) $message .= '<br/>Не совпадают контрольные суммы';
 	}
 
